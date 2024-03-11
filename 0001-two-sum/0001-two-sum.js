f221b559788e895eeb1976cfddea042f0bad7145
Smruti Karade <41608802+Smruti-25@users.noreply.github.com>
2024-03-12 05:00:14 +0530

    Time: 53 ms (88.53%), Space: 50.9 MB (35.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,11 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    for(let i = 0; i<nums.length - 1; i++){
-        for(let j = i+1; j<nums.length; j++){
-            if(nums[i] + nums[j] === target){
-                return [i,j];
-            }
+    const complement = {};
+    
+    for(let i = 0; i<nums.length; i++){
+        let diff = target - nums[i];
+        if(diff in complement){
+            return [i, complement[diff]];
+        } else {
+            complement[nums[i]] = i;
         }
     }
 };
