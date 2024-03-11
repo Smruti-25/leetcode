025b94985f880db2ba9faad435fa200f7470e08d
Smruti Karade <41608802+Smruti-25@users.noreply.github.com>
2024-03-12 05:24:39 +0530

    Time: 76 ms (48.56%), Space: 58.4 MB (81.21%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,19 +3,19 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let l = 0;
-    let r = 1;
     let maxProfit = 0;
     
-    while(r < prices.length){
-        if(prices[l] < prices[r]){
-            let profit = prices[r] - prices[l];
-            maxProfit = Math.max(profit, maxProfit);
+    let buy = 0;
+    let sell = 1;
+    
+    while(sell < prices.length){
+        let profit = prices[sell] - prices[buy];
+        maxProfit = Math.max(profit, maxProfit);
+        if(prices[sell] < prices[buy]){
+            buy = sell;
         }
-        else {
-            l = r;
-        }
-        r++;
+        sell++;
     }
-    return maxProfit
+    
+    return maxProfit;
 };
