588966c9b218631ca20edb2912d0f7b275f3b04c
Smruti Karade <41608802+Smruti-25@users.noreply.github.com>
2024-03-11 08:47:25 +0530

    Time: 47 ms (78.65%), Space: 48.5 MB (64.75%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.js
+++ 0054-spiral-matrix/0054-spiral-matrix.js
@@ -5,45 +5,35 @@
 var spiralOrder = function(matrix) {
     const result = [];
     let left = 0;
-    let right = matrix[0].length //row length or number of columns
+    let right = matrix[0].length //cols
     let top = 0;
-    let bottom = matrix.length //column length or number of rows
+    let bottom = matrix.length //rows
     
     while(left < right && top < bottom){
-        
-        //traverse row from left to right
         for(let i = left; i < right; i++){
             result.push(matrix[top][i]);
         }
-        //reduce the topmost boundary
         top = top + 1;
         
-        //traverse from top to bottom
         for(let i = top; i<bottom; i++){
-            result.push(matrix[i][right-1]);
+            result.push(matrix[i][right - 1])
         }
-        //reduce the rightmost boundary
-        right = right - 1
+        right = right - 1;
         
-        if(!(left < right && top < bottom)){ //incase its just one row
+        if(!(left < right && top < bottom)){
             break;
         }
         
-        //traverse from right to left
-        for(let i = right - 1; i >= left; i-- ){
-            result.push(matrix[bottom - 1][i]);
+        for(let i = right - 1; i >= left; i--){
+            result.push(matrix[bottom - 1][i])
         }
-        //reduce the bottom boundary
-        bottom = bottom - 1;
+        bottom = bottom -1;
         
-        //traverse from bottom to top
-        for(let i = bottom - 1; i >= top; i--){
-            result.push(matrix[i][left]);
+        for(let i = bottom - 1; i>= top; i--){
+            result.push([matrix[i][left]]);
         }
-        //reduce the left boundary
-        left = left + 1;
-        
+        left = left + 1
     }
     
-    return result
+    return result;
 };
