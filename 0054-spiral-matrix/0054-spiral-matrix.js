406bc2c1333d58d7517561d5e0c5d1670baa661d
Smruti Karade <41608802+Smruti-25@users.noreply.github.com>
2024-04-18 16:05:59 +0530

    Time: 54 ms (37.83%), Space: 49.1 MB (16.72%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.js
+++ 0054-spiral-matrix/0054-spiral-matrix.js
@@ -3,20 +3,23 @@
  * @return {number[]}
  */
 var spiralOrder = function(matrix) {
-    const result = [];
+    let res = [];
     let left = 0;
-    let right = matrix[0].length //cols
     let top = 0;
-    let bottom = matrix.length //rows
-    
+    let right = matrix[0].length; //number of columns
+    let bottom = matrix.length; //number of rows
+
     while(left < right && top < bottom){
+        
+        //top row
         for(let i = left; i < right; i++){
-            result.push(matrix[top][i]);
+            res.push(matrix[top][i]);
         }
         top = top + 1;
         
-        for(let i = top; i<bottom; i++){
-            result.push(matrix[i][right - 1])
+        //rightmost column
+        for(let i = top; i < bottom; i++){
+            res.push(matrix[i][right - 1]);
         }
         right = right - 1;
         
@@ -24,16 +27,18 @@ var spiralOrder = function(matrix) {
             break;
         }
         
+        //bottom row
         for(let i = right - 1; i >= left; i--){
-            result.push(matrix[bottom - 1][i])
+            res.push(matrix[bottom - 1][i]);
         }
-        bottom = bottom -1;
+        bottom = bottom - 1;
         
-        for(let i = bottom - 1; i>= top; i--){
-            result.push([matrix[i][left]]);
+        //first colums
+        for(let i = bottom - 1; i >= top; i--){
+            res.push(matrix[i][left]);
         }
-        left = left + 1
+        left = left + 1;
     }
-    
-    return result;
+
+    return res;
 };
